20b57c732dfb33a6feddf50a05c3b16048f98c84
#ZHANG CONG# <cong.zhang92@gmail.com>
2022-01-06 13:00:40 +0800

    progress
     - add network forward to rollout

--- rollout.py
+++ rollout.py
@@ -1,15 +1,12 @@
 import torch.cuda
-
 from pyjssp.simulators import Simulator
 import random
 import numpy
 import time
-import torch_geometric
-import networkx as nx
-from model import to_pyg
+from model import to_pyg, RLGNN
 
 
-def rollout(s, dev):
+def rollout(s, dev, net=None):
 
     s.reset()
     done = False
@@ -22,23 +19,21 @@ def rollout(s, dev):
         do_op_dict = s.get_doable_ops_in_dict()
         all_machine_work = False if bool(do_op_dict) else True
 
-        # g, _, _ = s.observe()
-        # print(list(nx.get_node_attributes(g, name='remain_time').values()))
-
         if all_machine_work:  # all machines are on processing. keep process!
             s.process_one_time()
         else:  # some of machine has possibly trivial action. the others not.
-            _, _, done, sub_list = s.flush_trivial_ops(reward='makespan')
+            _, _, done, sub_list = s.flush_trivial_ops(reward='makespan')  # flush the trivial action
             p_list += sub_list
             if done:
                 break  # env rollout finish
             g, r, done = s.observe(return_doable=True)
-            g_pre, g_suc, g_dis = to_pyg(g, dev)
+            # network forward goes here
+            if net is not None:
+                g_pre, g_suc, g_dis = to_pyg(g, dev)
+                pyg_graphs = {'pre': g_pre, 'suc': g_suc, 'dis': g_dis}
+                pyg_graphs = net(**pyg_graphs)
             op_id = s.transit()
             p_list.append(op_id)
-            g, r, done = s.observe(return_doable=True)
-            # for n in g.nodes:
-            #     print(n, g.nodes[n])
 
         if done:
             break  # env rollout finish
@@ -53,7 +48,8 @@ if __name__ == "__main__":
 
     dev = 'cuda' if torch.cuda.is_available() else 'cpu'
     s = Simulator(6, 6, verbose=False)
-    rollout(s, dev)
+    net = RLGNN().to(dev)
+    rollout(s, dev, net)
 
 
 
