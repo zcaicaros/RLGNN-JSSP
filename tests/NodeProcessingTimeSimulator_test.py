6f7807478b0b4ecb432fc84d706f22f65f6e0595
#ZHANG CONG# <sun.jing@ntu.edu.sg>
2022-01-17 13:47:20 +0800

    rm test folders and installed package

--- tests/NodeProcessingTimeSimulator_test.py
+++
@@ -1,9 +0,0 @@
-from pyjssp.simulators import NodeProcessingTimeSimulator
-from pyjssp.utils import pprint_graph
-
-if __name__ == "__main__":
-
-    sim = NodeProcessingTimeSimulator(2, 2)
-    g, r, done = sim.observe()
-
-    pprint_graph(g)
